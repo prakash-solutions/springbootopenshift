5e805d3e60ce459225d82ab768ceb1eb4eead18d
395233 <395233@PC285766.cts.com>
2018-03-09 15:24:03 +0530

    remove

--- springbootopeshift/src/main/java/com/cts/springbootopeshift/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.springbootopeshift.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cts.springbootopeshift.entity.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
